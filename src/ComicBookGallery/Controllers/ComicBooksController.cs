babcc1d72482f30c3f3c36ce8c4ce23da90fd73e
Gaurab Subedi <gsubedi2057@gmail.com>
2021-12-03 00:38:44 -0800

    Added data repository

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ComicBookGallery.Data;
+using ComicBookGallery.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,20 +10,26 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public ActionResult Detail()
+        private ComicBookRepository _comicBookRepository = new ComicBookRepository();
+        public ActionResult Detail(int? id)
         {
-            ViewBag.SeriesTitle = "The amazing spiderman";
-            ViewBag.IssueNumber = 700;
-            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
-            ViewBag.Artists = new string[]
+
+            //ViewBag.SeriesTitle = "The amazing spiderman";
+            //ViewBag.IssueNumber = 700;
+            //ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+            //ViewBag.Artists = new string[]
+            //{
+            //    "Script: Dan Slott",
+            //    "Pencils: Humberto Ramos",
+            //    "Inks: Victor Olazaba",
+            //    "Colors: Edgar Delgado",
+            //    "Letters: Chris Eliopoulos"
+            //};
+            if (id == null)
             {
-                "Script: Dan Slott",
-                "Pencils: Humberto Ramos",
-                "Inks: Victor Olazaba",
-                "Colors: Edgar Delgado",
-                "Letters: Chris Eliopoulos"
-            };
-            return View();
+                return HttpNotFound();
+            }
+            return View(_comicBookRepository.GetComicBook(id.Value));
         }
     }
 }
---
+++ src/ComicBookGallery/Models/ComicBook.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallery.Models
+{
+    public class ComicBook
+    {
+        public int Id { get; set; }
+        public string SeriesTitle { get; set; }
+        public int  IssueNumber { get; set; }
+        public String DescriptionHtml { get; set; }
+        public Artist[] Artists { get; set; }
+        public bool Favorite { get; set; }
+
+        public string DisplayText
+        {
+            get
+            {
+                return SeriesTitle + " #" + IssueNumber;
+            }
+        }
+
+        public string CoverImageFileName { get; } = "the-amazing-spider-man-700.jpg";
+    }
+}
